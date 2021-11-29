fbdaf6db055eb5260c626abd084c92bd844bfeb3
carloskauan <carloskauan190@gmail.com>
2021-11-29 17:07:22 -0300

    Atualizando Mvc

---
+++ Controllers/ArmasController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RpgMvc.Controllers
+{
+    public class ArmasController : Controller
+    {
+        public string uriBase = "http://CarlosKauan.somee.com/RpgApi/Armas/";    
+
+        //Criar - Post
+        //Selecionar - get
+        //Editar - Put
+        //Deletar - Post
+
+        //1. Criar a classe ArmaViewModel.cs na pasta Models, com as propriedades similares a classe Arma do projeto de API. - V -
+        
+        // -------------------------------
+
+        // -------------------------------
+
+        //2. Criar a controller ArmasController.cs na pasta Controllers e todos os métodos necessários para consumir a API publicada no Somee. - -
+
+        // -------------------------------
+
+        // -------------------------------
+
+        
+        //3. Criar a pasta Armas dentro da pasta Views e dentro dela as views Index, Create, Details e Edit.
+            
+            /*
+            3.1 Um detalhe muito importante é que a tabela de armas precisa do ID de um personagem para vincular o personagem a arma. 
+            Você pode utilizar o método que lista os personagens, e carregar em um DropDownList como fizemos nas etapas para carregar as habilidades 
+            na disputa com habilidades. - -
+            */
+        
+        /*
+        // -------------------------------
+
+        // -------------------------------
+       
+        4. Fazer o print do funcionamento de todas as Views com as devidas mensagens referente as operações,
+         salvar em um arquivo e postar na tarefa criada Teams.
+         */
+        
+        // -------------------------------
+        
+        // -------------------------------
+
+
+    }
+}
